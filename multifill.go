8747e6fe0258f4ac72e2684b50d41e14865860c0
Kamil Rezac <kamil.rezac@concur.com>
2016-08-08 17:42:35 +0200

    wrapper counting tests

--- multifill.go
+++ multifill.go
@@ -3,8 +3,36 @@ package fako
 import (
 	"fmt"
 	"reflect"
-	"strings"
 )
+
+// RunNilMultiTest calls the testFunc on different version of input struct with nil/not-nil initialized pointers
+// data needs to be pointer so the routine can change the data
+func RunNilMultiTest(data interface{}, testFunc func() error, canPanic bool) (total, failed int, errs []error) {
+	testfn := func() error {
+		if !canPanic {
+			defer func() {
+				if r := recover(); r != nil {
+					err := fmt.Errorf("Panic happened: %v", r)
+					errs = append(errs, err)
+					failed++
+				}
+			}()
+		}
+		total++
+		err := testAndGen(data, testFunc)
+		if err != nil {
+			errs = append(errs, err)
+			failed++
+		}
+		return nil // we want to continue with next test
+	}
+
+	err := testAndGen(data, testfn)
+	if err != nil { // out of test function error
+		errs = append(errs, err)
+	}
+	return
+}
 
 func testAndGen(currentData interface{}, testFunc func() error) error {
 
@@ -16,12 +44,12 @@ func testAndGen(currentData interface{}, testFunc func() error) error {
 
 	// evaluate what's the actual type in the parameter
 	dType := reflect.TypeOf(currentData)
-	fmt.Printf("Type of data is %v\n", dType)
+	//fmt.Printf("Type of data is %v\n", dType)
 
 	// if the parameter is pointer, get the actual data
 	if dType.Kind() == reflect.Ptr {
 		dType = dType.Elem()
-		fmt.Printf("Pointer type, evaluating referenced type %v\n", dType)
+		//fmt.Printf("Pointer type, evaluating referenced type %v\n", dType)
 	}
 
 	if dType.Kind() == reflect.Struct {
@@ -31,18 +59,24 @@ func testAndGen(currentData interface{}, testFunc func() error) error {
 		for i := 0; i < dType.NumField(); i++ {
 			fieldValue := dValue.Field(i) // this is type Value
 			fieldType := fieldValue.Type()
-			fmt.Printf("Field %d: type: %v, value: %v\n", i, fieldType, fieldValue)
+			//fmt.Printf("Field %d: type: %v, value: %v\n", i, fieldType, fieldValue)
+
+			if !fieldValue.CanSet() {
+				fmt.Println("Field can't be set")
+				continue
+			}
+
 			//fmt.Printf("Struct field type %v: %#v\n", field.Kind(), field)
 			if fieldValue.Kind() == reflect.Ptr {
 				// create new object
-				fmt.Printf("Pointer type found, about to create new element of type %v\n", fieldType)
+				//fmt.Printf("Pointer type found, about to create new element of type %v\n", fieldType)
 				newValPtr := reflect.New(fieldType.Elem())
 				fieldValue.Set(newValPtr)
 				// fmt.Printf("Created new element of type %v: %#v\n", newValPtr.Type(), newValPtr)
 				// recurse for newly created data
 				err := testAndGen(newValPtr.Interface(), testFunc)
 				if err != nil {
-					fmt.Printf("Error during test: %v\n", err)
+					//fmt.Printf("Error during test: %v\n", err)
 					return err
 				}
 			} else if fieldValue.Kind() == reflect.Slice {
@@ -50,10 +84,10 @@ func testAndGen(currentData interface{}, testFunc func() error) error {
 				itemType := fieldType.Elem()
 
 				var newValPtr reflect.Value
-				fmt.Printf("Slice of type %v found, about to insert one item\n", itemType)
+				//fmt.Printf("Slice of type %v found, about to insert one item\n", itemType)
 				if itemType.Kind() == reflect.Ptr {
 					itemType = itemType.Elem()
-					fmt.Printf("Pointer type, evaluating referenced type %v\n", itemType)
+					// fmt.Printf("Pointer type, evaluating referenced type %v\n", itemType)
 					// append new item
 					newValPtr = reflect.New(itemType)
 					fieldValue.Set(reflect.Append(fieldValue, newValPtr))
@@ -68,19 +102,21 @@ func testAndGen(currentData interface{}, testFunc func() error) error {
 				// recurse for newly created data
 				err := testAndGen(newValPtr.Interface(), testFunc)
 				if err != nil {
-					fmt.Printf("Error during test: %v\n", err)
+					// fmt.Printf("Error during test: %v\n", err)
 					return err
 				}
 			} else if fieldValue.Kind() == reflect.Struct {
+				/* should be covered by CanSet check
 				if strings.HasPrefix(fieldType.String(), "time.") {
 					// if you try to go thru time.Time, reflect panics because of unexported fields
 					fmt.Println("do not dive into time package")
 					continue
 				}
-				fmt.Printf("non-pointer struct, just recurse for %v\n", fieldType)
+				*/
+				// fmt.Printf("non-pointer struct, just recurse for %v\n", fieldType)
 				err := testAndGen(fieldValue.Addr().Interface(), testFunc)
 				if err != nil {
-					fmt.Printf("Error during test: %v\n", err)
+					// fmt.Printf("Error during test: %v\n", err)
 					return err
 				}
 			}
